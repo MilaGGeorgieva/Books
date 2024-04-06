48ea4d892be94daab9c684d67a4cab2ac143ac60
MilaGGeorgieva <milaggeorgieva@gmail.com>
2024-04-06 20:42:08 +0300

    Added update and delete a book api tests

--- api.test.js
+++ api.test.js
@@ -37,12 +37,41 @@ describe('Books API', () => {
     it('should GET all books', (done) => {
         chai.request(server)
             .get('/books')
-            .end((err, resp) =>{
+            .end((err, resp) => {
                 if (err) {
                     return done(err)
                 }
                 expect(resp.statusCode).to.equal(200);
                 expect(resp.body).to.be.a('array');
+                done();
+            });
+    });
+
+    it('should PUT an existing book', (done) => {
+        const updatedBook = { id: "1", title: "DevOps magic Update", author: "Peter Peterson Updatious" };
+        chai.request(server)
+            .put('/books/1')
+            .send(updatedBook)
+            .end((err, resp) => {
+                if (err) {
+                    return done(err)
+                }
+                expect(resp.statusCode).to.equal(200);
+                expect(resp.body).to.be.a('object');
+                expect(resp.body.title).to.equal('DevOps magic Update');
+                expect(resp.body.author).to.equal('Peter Peterson Updatious');
+                done();
+            });
+    });
+
+    it('should DELETE an existing book', (done) => {
+        chai.request(server)
+            .delete('/books/1')
+            .end((err, resp) => {
+                if (err) {
+                    return done(err)
+                }
+                expect(resp.statusCode).to.equal(204);
                 done();
             });
     });
